a8c7e1b4086131037d5ae2d83e5ee698b3580681
Michael Highsmith <michaelhighsmith11@gmail.com>
2018-02-24 15:42:05 -0600

    Plant anims and watering

---
+++ Assets/NextPlant.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextPlant : MonoBehaviour
+{
+
+    public GameObject[] Models;
+    private int i = 0;
+    public GameObject RainCloud;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void WaterPlant()
+    {
+        StartCoroutine(makeCloud());
+
+    }
+
+    public IEnumerator makeCloud()
+    {
+        RainCloud.SetActive(true);
+        yield return new WaitForSeconds(4);
+        RainCloud.SetActive(false);
+    }
+
+    public void Next()
+    {
+        if (Models[i].activeInHierarchy == true && i < 1)
+        {
+            Models[i].SetActive(false);
+            Models[i + 1].SetActive(true);
+            i++;
+        }
+    }
+
+    public void Prev()
+    {
+        if (Models[i].activeInHierarchy == true && i > 0)
+        {
+            Models[i].SetActive(false);
+            Models[i - 1].SetActive(true);
+            i--;
+        }
+    }
+}
